c5c333c38b1b8accfb339829c7895b61bec1171f
908169 <908169@ISS-2199758.tawe.swan.ac.uk>
2018-11-20 22:36:19 +0000

    Improved the work but doesn't work

--- practice1.java
+++ practice1.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
 
-public class Resource {
+public class practice1 {
 
 		public static void main(String[] args) {
+			Scanner in = new Scanner(System.in);
 			System.out.println("Hello Cameron");
+			System.out.println("How are you?");
+			String answer = in.nextLine();
+			if (answer == "fine") {
+				System.out.println("Good good");
+				
+			} else {
+				System.out.println("Fuck you");
+			}
+			
 			
 		}
 }
